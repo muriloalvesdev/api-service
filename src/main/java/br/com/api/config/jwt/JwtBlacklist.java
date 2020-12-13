9e4697574dafa83ce599f70f2f26146ca843743d
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-12-13 15:30:56 -0300

    config jwt

---
+++ src/main/java/br/com/api/config/jwt/JwtBlacklist.java
@@ -0,0 +1,48 @@
+package br.com.api.config.jwt;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Scope("singleton")
+public class JwtBlacklist {
+  private static Map<String, String> blacklist = new ConcurrentHashMap<>();
+  private String jwtSecret;
+
+  public synchronized void add(String token) {
+    if (!blacklist.containsKey(token)) {
+      blacklist.putIfAbsent(token, String.valueOf(System.currentTimeMillis()));
+    } else {
+      log.info("TOKEN JA FOI INVALIDADO, TOKEN [{}] ", token);
+    }
+  }
+
+  public boolean check(String token) {
+    return StringUtils.isNotBlank(token) ? blacklist.containsKey(token) : false;
+  }
+
+  public synchronized void cleanBlacklist() {
+    for (String token : blacklist.keySet()) {
+      try {
+        getDateExpirationToken(token);
+      } catch (ExpiredJwtException e) {
+        blacklist.remove(token);
+        continue;
+      }
+    }
+  }
+
+  private LocalDateTime getDateExpirationToken(String token) {
+    return Jwts.parser().setSigningKey(this.jwtSecret).parseClaimsJws(token).getBody()
+        .getExpiration().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+  }
+}
---
+++ src/main/java/br/com/api/config/jwt/JwtProvider.java
@@ -0,0 +1,63 @@
+package br.com.api.config.jwt;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import br.com.api.user.dto.UserDTO;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+  @Value("${security.app.jwtSecret}")
+  private String jwtSecret;
+
+  @Value("${security.app.jwtExpiration}")
+  private int jwtExpiration;
+
+  public String generateJwtToken(Authentication authentication) {
+
+    UserDTO userPrincipal = (UserDTO) authentication.getPrincipal();
+
+    return Jwts.builder().setSubject((userPrincipal.getEmail())).setIssuedAt(new Date())
+        .claim("username", userPrincipal.getName())
+        .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
+        .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+  }
+
+  public String getUserNameFromJwtToken(String token) {
+    return this.getUserInformation(token).getSubject();
+  }
+
+  public Claims getUserInformation(String token) {
+    return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+  }
+
+  public boolean validateJwtToken(String authToken) {
+    try {
+      Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+      return true;
+    } catch (SignatureException e) {
+      log.error("Invalid JWT signature -> Message: {} ", e);
+    } catch (MalformedJwtException e) {
+      log.error("Invalid JWT token -> Message: {}", e);
+    } catch (ExpiredJwtException e) {
+      log.error("Expired JWT token -> Message: {}", e);
+    } catch (UnsupportedJwtException e) {
+      log.error("Unsupported JWT token -> Message: {}", e);
+    } catch (IllegalArgumentException e) {
+      log.error("JWT claims string is empty -> Message: {}", e);
+    }
+
+    return false;
+  }
+}
