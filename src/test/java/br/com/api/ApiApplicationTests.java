b10d4c59fea3089845d645f37308782b39c00579
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-12-13 15:37:13 -0300

    checkstyle

--- src/test/java/br/com/api/ApiApplicationTests.java
+++ src/test/java/br/com/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.br.api;
+package br.com.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
