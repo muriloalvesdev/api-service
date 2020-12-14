0fc74212abdda7a2e3fe31dfa1e3740e8f2466e6
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-12-14 00:39:25 -0300

    apis for crud user

--- src/main/java/br/com/api/controller/UserController.java
+++ src/main/java/br/com/api/controller/UserController.java
@@ -1,16 +1,23 @@
 package br.com.api.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.api.user.dto.LoginDTO;
 import br.com.api.user.dto.RegisterDTO;
+import br.com.api.user.dto.UserDTO;
 import br.com.api.user.model.AccessToken;
 import br.com.api.user.model.User;
 import br.com.api.user.service.UserService;
@@ -20,9 +27,33 @@ import lombok.AllArgsConstructor;
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/auth")
 @AllArgsConstructor
-public class AuthenticationController {
+class UserController {
 
   private UserService userService;
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @DeleteMapping("delete{email}")
+  public void delete(@RequestParam(name = "email") String email) {
+    this.userService.delete(email);
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/update")
+  public ResponseEntity<UserDTO> update(@RequestBody RegisterDTO registerDTO) {
+    return ResponseEntity.ok(this.userService.update(registerDTO));
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @GetMapping("/find")
+  public ResponseEntity<List<UserDTO>> find() {
+    return ResponseEntity.ok(this.userService.find());
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @GetMapping("/find{email}")
+  public ResponseEntity<UserDTO> find(@RequestParam(name = "email") String email) {
+    return ResponseEntity.ok(this.userService.find(email));
+  }
 
   @PostMapping("/login")
   public ResponseEntity<AccessToken> authenticateUser(@Validated @RequestBody LoginDTO loginData) {
@@ -32,6 +63,6 @@ public class AuthenticationController {
   @PostMapping("/register")
   public ResponseEntity<User> registerUser(@Valid @RequestBody RegisterDTO registerData) {
     return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-        .buildAndExpand(this.userService.registerUser(registerData).getId()).toUri()).build();
+        .buildAndExpand(this.userService.register(registerData).getId()).toUri()).build();
   }
 }
