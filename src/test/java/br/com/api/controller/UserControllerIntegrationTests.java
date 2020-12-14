0351771c960e0c21f560d10cf0b8e257bcbe3d84
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2020-12-14 00:40:05 -0300

    adding remaining user crud to the integration test

--- src/test/java/br/com/api/controller/UserControllerIntegrationTests.java
+++ src/test/java/br/com/api/controller/UserControllerIntegrationTests.java
@@ -1,7 +1,12 @@
 package br.com.api.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,7 +20,9 @@ import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import br.com.api.constants.UserConstantsForTests;
 import br.com.api.provider.RegisterDTOProviderTests;
+import br.com.api.user.dto.LoginDTO;
 import br.com.api.user.dto.RegisterDTO;
+import br.com.api.user.model.AccessToken;
 
 @SpringBootTest
 class UserControllerIntegrationTests implements UserConstantsForTests {
@@ -32,15 +39,54 @@ class UserControllerIntegrationTests implements UserConstantsForTests {
 
   @ParameterizedTest
   @ArgumentsSource(RegisterDTOProviderTests.class)
-  void shouldRegisterWithSucess(RegisterDTO registerData) throws Exception {
+  void shouldCrudUser(RegisterDTO registerData) throws Exception {
+    LoginDTO dto = new LoginDTO();
+    dto.setEmail(registerData.getEmail());
+    dto.setPassword(registerData.getPassword());
+
     this.mockMvc
         .perform(post(PATH_AUTH.concat("register")).content(createJsonRegisterData(registerData))
             .contentType(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(status().isCreated()).andExpect(header().exists("Location"));
+
+    String token = this.mockMvc
+        .perform(post(PATH_AUTH.concat("login")).content(createJsonLogin(dto))
+            .contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andReturn().getResponse().getContentAsString();
+    AccessToken accessToken = MAPPER.readValue(token, AccessToken.class);
+
+    this.mockMvc
+        .perform(get(PATH_AUTH.concat("find?email=").concat(EMAIL))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .header("Authorization", "Bearer " + accessToken.getAccessToken()))
+        .andExpect(status().isOk()).andExpect(jsonPath("$.[0].name", is(FIRST_NAME.toUpperCase())))
+        .andExpect(jsonPath("$.[0].lastName", is(LAST_NAME.toUpperCase())))
+        .andExpect(jsonPath("$.[0].email", is(EMAIL)));
+
+    this.mockMvc
+        .perform(get(PATH_AUTH.concat("find")).contentType(MediaType.APPLICATION_JSON_VALUE)
+            .header("Authorization", "Bearer " + accessToken.getAccessToken()))
+        .andExpect(status().isOk());
+
+    this.mockMvc
+        .perform(put(PATH_AUTH.concat("update")).content(createJsonRegisterData(registerData))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .header("Authorization", "Bearer " + accessToken.getAccessToken()))
+        .andExpect(status().isOk());
+
+    this.mockMvc
+        .perform(delete(PATH_AUTH.concat("delete?email=").concat(EMAIL))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .header("Authorization", "Bearer " + accessToken.getAccessToken()))
+        .andExpect(status().isOk());
   }
 
   private String createJsonRegisterData(RegisterDTO registerData) throws JsonProcessingException {
     return MAPPER.writeValueAsString(registerData);
+  }
+
+  private String createJsonLogin(LoginDTO loginDTO) throws JsonProcessingException {
+    return MAPPER.writeValueAsString(loginDTO);
   }
 
 }
